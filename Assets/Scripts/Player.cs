97b403fed728039d5c053f02e7f401d427816574
Luis Nasser <luishnc93@gmail.com>
2020-10-28 23:45:01 -0400

    Implemented player controller

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private CharacterController controller;
+    [SerializeField]
+    private float speed = 3.5f;
+    private float gravity = 9.81f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        calculateMovement();
+
+    }
+
+    void calculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+        Vector3 velocity = direction * speed;
+        velocity.y -= gravity;
+        velocity = transform.transform.TransformDirection(velocity);
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
